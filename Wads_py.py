b91054f0d2e2c84c92cdeb13d93706ff08dbb5f5
Josh Galofaro <galo9664@fredonia.edu>
2011-10-17 21:10:35 -0400

    Rough draft of the Python version.

---
+++ Wads_py.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+import sys
+
+def wadsworth(original):  
+  if len(sys.argv) != 0:  phrase = ' '.join(original[1:])
+  else: phrase = original
+  output = phrase[int(len(phrase)-len(phrase)*.70):]
+  return output
+
+if __name__ == '__main__':
+  print wadsworth(sys.argv)
+
