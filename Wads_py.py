ae63377475aa4d02c7706599ed3006f417007a13
Josh Galofaro <galo9664@fredonia.edu>
2014-03-05 17:30:19 -0500

    Fixed indentation mistake

--- Wads_py.py
+++ Wads_py.py
@@ -3,9 +3,14 @@
 import sys
 
 def wadsworth(original):  
-  if len(sys.argv) != 0:  phrase = ' '.join(original[1:])
-  else: phrase = original
+
+  if len(sys.argv) != 0:
+    phrase = ' '.join(original[1:])
+  else:
+    phrase = original
+
   output = phrase[int(len(phrase)-len(phrase)*.70):]
+
   return output
 
 if __name__ == '__main__':
